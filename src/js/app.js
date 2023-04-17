300098539942e532ceb77eda5427be693d262523
wee-owl <kem-cv@yandex.ru>
2023-04-17 13:01:20 +0300

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line no-console
+console.log('app worked');
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader, 'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+  ],
+};
